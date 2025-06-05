fe0fa61f1caf137b9cf0d82b6fa3f011bd932008
Olude David <odavidolumide@gmail.com>
2025-06-05 11:19:17 +0200

    add courses

--- src/ui/components/review.tsx
+++ src/ui/components/review.tsx
@@ -1,5 +1,6 @@
 export default function Review() {
   return (
+    // todo
     <div className="w-full grid grid-cols-1 place-items-center">
       <div className="w-3/5 grid grid-cols-1 bg-red-500">
         <div>review</div>
